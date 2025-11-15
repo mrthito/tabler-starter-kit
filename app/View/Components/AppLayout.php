3312288728a174cc2105893f394d1795182f217f
Prashant Rijal <prashant@oklaravel.com>
2025-11-15 09:39:46 +0545

    Refactor AppLayout and GuestLayout components to accept page title and layout type. Update render methods to dynamically return views based on layout type, enhancing flexibility for web and admin layouts.

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -7,11 +7,20 @@ use Illuminate\View\View;
 
 class AppLayout extends Component
 {
+    public function __construct(
+        public string $page,
+        public string $layout = 'web',
+    ) {}
+
     /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
     {
-        return view('layouts.app');
+        $title = $this->page ? $this->page . ' - ' . config('app.name') : config('app.name');
+        return match ($this->layout) {
+            'web' => view('layouts.app', compact('title')),
+            'admin' => view('admins.layouts.app', compact('title')),
+        };
     }
 }
--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -7,11 +7,20 @@ use Illuminate\View\View;
 
 class GuestLayout extends Component
 {
+    public function __construct(
+        public string $page,
+        public string $layout = 'web',
+    ) {}
+
     /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
     {
-        return view('layouts.guest');
+        $title = $this->page ? $this->page . ' - ' . config('app.name') : config('app.name');
+        return match ($this->layout) {
+            'web' => view('layouts.guest', compact('title')),
+            'admin' => view('admins.layouts.guest', compact('title')),
+        };
     }
 }
